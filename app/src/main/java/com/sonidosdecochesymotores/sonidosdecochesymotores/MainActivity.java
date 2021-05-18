b8cba9f29c4be423a188aeff6d9e134f37dfc06a
gurzelai <gorkaurzelai2000@gmail.com>
2021-05-19 01:15:38 +0200

    1.6 anuncio mas grande

--- app/src/main/java/com/sonidosdecochesymotores/sonidosdecochesymotores/MainActivity.java
+++ app/src/main/java/com/sonidosdecochesymotores/sonidosdecochesymotores/MainActivity.java
@@ -1,7 +1,5 @@
 package com.sonidosdecochesymotores.sonidosdecochesymotores;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.AssetManager;
@@ -11,12 +9,13 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -116,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
     private void pantallaCompleta() {
         getSupportActionBar().hide();
         View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY; //View.SYSTEM_UI_FLAG_FULLSCREEN
         decorView.setSystemUiVisibility(uiOptions);
     }
 
